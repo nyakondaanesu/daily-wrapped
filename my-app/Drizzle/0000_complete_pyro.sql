65a9db073d657e11cbe730eab66d9186fc605c8f
unknown <nyakondaa@africau.edu>
2025-01-12 13:48:11 +0200

    final version

--- my-app/Drizzle/0000_complete_pyro.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "tokens_table" (
-	"id" text PRIMARY KEY NOT NULL,
-	"access_token" text NOT NULL,
-	"refresh_token" text NOT NULL,
-	"expire_at" integer NOT NULL
-);
--- my-app/Drizzle/migrations/0000_tense_ultimo.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "tokens_table" (
-	"id" text NOT NULL,
-	"access_token" text PRIMARY KEY NOT NULL,
-	"refresh_token" text NOT NULL,
-	"expire_at" integer NOT NULL
-);
